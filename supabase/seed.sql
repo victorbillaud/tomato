40bda74c63be0c0a20e62c7edae4e5cd765faf52
victorbillaud <victorbillaud@gmail.com>
2023-11-10 16:34:09 +0100

    add seed script and user init migration

---
+++ supabase/seed.sql
@@ -0,0 +1,67 @@
+-- supabase/seed.sql
+--
+-- create test users
+INSERT INTO
+    auth.users (
+        instance_id,
+        id,
+        aud,
+        role,
+        email,
+        encrypted_password,
+        email_confirmed_at,
+        recovery_sent_at,
+        last_sign_in_at,
+        raw_app_meta_data,
+        raw_user_meta_data,
+        created_at,
+        updated_at,
+        confirmation_token,
+        email_change,
+        email_change_token_new,
+        recovery_token
+    ) (
+        select
+            '00000000-0000-0000-0000-000000000000',
+            uuid_generate_v4 (),
+            'authenticated',
+            'authenticated',
+            'user' || (ROW_NUMBER() OVER ()) || '@example.com',
+            crypt ('password123', gen_salt ('bf')),
+            current_timestamp,
+            current_timestamp,
+            current_timestamp,
+            '{"provider":"email","providers":["email"]}',
+            format('{"full_name":"%s", "avatar_url":"%s"}', 'User ' || (ROW_NUMBER() OVER ()), 'https://avatars.dicebear.com/api/initials/' || (ROW_NUMBER() OVER ()) || '.svg') :: jsonb,
+            current_timestamp,
+            current_timestamp,
+            '',
+            '',
+            '',
+            ''
+        FROM
+            generate_series(1, 10)
+    );
+
+-- test user email identities
+INSERT INTO
+    auth.identities (
+        id,
+        user_id,
+        identity_data,
+        provider,
+        last_sign_in_at,
+        created_at,
+        updated_at
+    ) (
+        select
+            uuid_generate_v4 (),
+            id,
+            format('{"sub":"%s","email":"%s"}', id :: text, email) :: jsonb,
+            'email',
+            current_timestamp,
+            current_timestamp,
+            current_timestamp
+        from
+            auth.users
+    );
