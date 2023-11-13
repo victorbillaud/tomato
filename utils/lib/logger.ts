b880941880136a2b85cdc36047de5bd505617974
victorbillaud <victorbillaud@gmail.com>
2023-11-13 14:50:27 +0100

    wip auth init

---
+++ utils/lib/logger.ts
@@ -0,0 +1,51 @@
+import winston from "winston";
+
+const logFileName:string = 'logs/error.log';
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+}
+
+const colors: winston.config.AbstractConfigSetColors  = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white',
+}
+
+function level() : string {
+    const env: string | undefined = process.env.NODE_ENV || 'development';
+    const isDevelopment = env === 'development';
+    return isDevelopment ? 'debug' : 'warn';
+}
+
+winston.addColors(colors);
+
+const format = winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+    winston.format.colorize({ all: true }),
+    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+)
+
+const transports: Array<any> = [
+    new winston.transports.Console(),
+    new winston.transports.File({
+        filename: logFileName,
+        level: 'error',
+    }),
+    new winston.transports.File({ filename: logFileName }),
+];
+
+const Logger: winston.Logger = winston.createLogger({
+    level: level(),
+    levels,
+    format,
+    transports
+});
+
+export default Logger;
---
+++ web/components/AuthButton.tsx
@@ -0,0 +1,40 @@
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+
+export default async function AuthButton() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  const signOut = async () => {
+    'use server'
+
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+    await supabase.auth.signOut()
+    return redirect('/auth/login')
+  }
+
+  return user ? (
+    <div className="flex items-center gap-4">
+      Hey, {user.email}!
+      <form action={signOut}>
+        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+          Logout
+        </button>
+      </form>
+    </div>
+  ) : (
+    <Link
+      href="/auth/login"
+      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+    >
+      Login
+    </Link>
+  )
+}
