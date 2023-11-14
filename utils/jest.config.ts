137488f7dd5c72c19f97ab9686351180cc7b4d58
victorbillaud <victorbillaud@gmail.com>
2023-11-14 15:18:05 +0100

    setup eslint and prettier for utils directory

---
+++ utils/jest.config.ts
@@ -0,0 +1,14 @@
+import { pathsToModuleNameMapper } from 'ts-jest'
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+import type { JestConfigWithTsJest } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  roots: ['./'],
+  modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */),
+}
+
+export default jestConfig
