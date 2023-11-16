27b64062aadcdf46c5140b95a7111a783008a54c
Thomas <tomspace.800@gmail.com>
2023-11-16 12:06:06 +0100

    fix hover when disable

--- web/app/ui/page.tsx
+++ web/app/ui/page.tsx
@@ -3,13 +3,14 @@ import { InputText } from '@/components/common/input/InputText';
 import { StyledLink } from '@/components/common/link';
 import { Text } from '@/components/common/text';
 
+// Storybook
 export default async function Index() {
   return (
     <div className='animate-in flex w-full flex-col gap-5 px-3 opacity-0'>
       <Text variant={'h1'} className='text-center'>
         UI/Theme
       </Text>
-      <div className='flex gap-10'>
+      <div className='flex flex-wrap gap-10'>
         <div className='flex flex-col justify-end space-y-8'>
           <Button variant={'primary'} text='Click me'></Button>
           <Button variant={'secondary'} text='Click me'></Button>
@@ -21,25 +22,25 @@ export default async function Index() {
           <Button variant={'tertiary'} disabled text='Click me'></Button>
         </div>
         <div className='flex flex-col justify-end space-y-8'>
-          <StyledLink href={'/'} text='Link' variant='primary'></StyledLink>
-          <StyledLink href={'/'} text='Link' variant='secondary'></StyledLink>
-          <StyledLink href={'/'} text='Link' variant='tertiary'></StyledLink>
+          <StyledLink href={'/ui'} text='Link' variant='primary'></StyledLink>
+          <StyledLink href={'/ui'} text='Link' variant='secondary'></StyledLink>
+          <StyledLink href={'/ui'} text='Link' variant='tertiary'></StyledLink>
         </div>
         <div className='flex flex-col justify-end space-y-8'>
           <StyledLink
-            href={'/'}
+            href={'/ui'}
             text='Link'
             variant='primary'
             disabled
           ></StyledLink>
           <StyledLink
-            href={'/'}
+            href={'/ui'}
             text='Link'
             variant='secondary'
             disabled
           ></StyledLink>
           <StyledLink
-            href={'/'}
+            href={'/ui'}
             text='Link'
             variant='tertiary'
             disabled
--- web/components/common/link/Link.tsx
+++ web/components/common/link/Link.tsx
@@ -7,9 +7,10 @@ export const StyledLink: FunctionComponent<ILinkProps> = ({
   variant = 'primary',
   ...props
 }) => {
+  const buttonClasses = getLinkClass(variant);
   const linkClassName = classNames(
-    getLinkClass(variant),
-    props.disabled ? 'opacity-50' : '',
+    buttonClasses.base,
+    props.disabled ? 'opacity-50 cursor-default' : buttonClasses.interact,
     'flex min-w-max items-center justify-center h-10 py-1 m-1 px-4',
     'transition-all'
   );
@@ -23,10 +24,22 @@ export const StyledLink: FunctionComponent<ILinkProps> = ({
 const getLinkClass = (style: LinkVariant) => {
   switch (style) {
     case 'primary':
-      return 'rounded-md border border-transparent bg-primary-900 font-medium text-gray-100 shadow-sm hover:bg-primary-950 focus:outline-none focus:ring-2 focus:ring-primary-950 focus:ring-offset-2';
+      return {
+        base: 'rounded-md border border-transparent bg-primary-900 font-medium text-gray-100 shadow-sm',
+        interact:
+          'hover:bg-primary-950 focus:outline-none focus:ring-2 focus:ring-primary-950 focus:ring-offset-2',
+      };
     case 'secondary':
-      return 'rounded-md border dark:border-gray-100 bg-gray-100 dark:bg-transparent font-medium text-gray-700 dark:text-gray-100 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200';
+      return {
+        base: 'rounded-md border dark:border-gray-100 bg-gray-100 dark:bg-transparent font-medium text-gray-700 dark:text-gray-100 shadow-sm',
+        interact:
+          'hover:bg-gray-200 dark:hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200',
+      };
     case 'tertiary':
-      return 'rounded-md border border-transparent bg-transparent font-medium text-gray-700 dark:text-gray-200 hover:text-gray-700 focus:outline-none';
+      return {
+        base: 'rounded-md border border-transparent bg-transparent font-medium text-gray-700 dark:text-gray-200',
+        interact:
+          'hover:text-gray-500 dark:hover:text-gray-500 focus:outline-none',
+      };
   }
 };
