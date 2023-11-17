0fee27549897e89ae4ed3c741f40a034c975c92e
victorbillaud <victorbillaud@gmail.com>
2023-11-17 21:17:35 +0100

    hostname fix

--- web/components/AuthProviders.tsx
+++ web/components/AuthProviders.tsx
@@ -8,10 +8,12 @@ export default function AuthProviders() {
   const supabase = createClient();
 
   const getURL = () => {
-    let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      'http://localhost:3000/';
+    let url = window.location.hostname;
+
+    if (url == 'localhost') {
+      url = 'http://localhost:3000';
+    }
+
     // Make sure to include `https://` when not localhost.
     url = url.includes('http') ? url : `https://${url}`;
     // Make sure to include /auth/callback at the end.
