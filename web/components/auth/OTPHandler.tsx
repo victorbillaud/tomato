248d19519fc994c142a55d22cf729727c5dcce63
victorbillaud <victorbillaud@gmail.com>
2023-11-23 19:03:23 +0100

    wip

--- web/components/auth/OTPHandler.tsx
+++ web/components/auth/OTPHandler.tsx
@@ -1,15 +1,17 @@
 'use client';
 
-import { SubmitButton } from '@/components/common/button';
+import { Button, SubmitButton } from '@/components/common/button';
 import { Card } from '@/components/common/card';
 import { InputText } from '@/components/common/input';
 import { Text } from '@/components/common/text';
 import { createClient } from '@/utils/supabase/client';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import OtpInput from 'react-otp-input';
 
 export const OTPHandler = () => {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [otp, setOTP] = useState('');
   const [message, setMessage] = useState('');
   const [otpSent, setOtpSent] = useState(false);
@@ -19,24 +21,6 @@ export const OTPHandler = () => {
 
   const supabase = createClient();
   const router = useRouter();
-
-  const userAlreadyExists = async (email: string) => {
-    const { data, error } = await supabase.auth.signUp({
-      email: email,
-      password: 'example-password',
-    });
-
-    if (error) {
-      console.error(error);
-      return true;
-    }
-
-    if (data.user) {
-      return true;
-    }
-
-    return false;
-  };
 
   const sendOTP = async (email: string) => {
     try {
@@ -70,11 +54,37 @@ export const OTPHandler = () => {
 
     setUserExist(await userAlreadyExists(email));
 
-    if (!userExist) {
+    if (userExist) {
       sendOTP(email);
     }
 
     setLoginButtonTrigger(true);
+  };
+
+  const handleLoginWithPassword = async (event: any) => {
+    event.preventDefault();
+    setMessage('');
+
+    try {
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+
+      if (error) {
+        setMessage(error.message);
+        throw error;
+      }
+
+      if (user) {
+        router.refresh();
+      }
+    } catch (error) {
+      setMessage('An error occurred. Please try again.');
+    }
   };
 
   const handleVerifyOTP = async (event: any) => {
@@ -102,8 +112,8 @@ export const OTPHandler = () => {
   };
 
   return (
-    <div className='flex w-full flex-col items-center justify-start space-y-5'>
-      {!loginButtonTrigger ? (
+    <div className='flex w-full flex-col items-center justify-start space-y-3'>
+      {!loginButtonTrigger && !otpSent ? (
         <form onSubmit={handleLogin} className='my-5 flex w-full'>
           <div className='w-full space-y-3 rounded-lg'>
             <InputText
@@ -124,7 +134,86 @@ export const OTPHandler = () => {
             />
           </div>
         </form>
-      ) : null}
+      ) : userExist && !otpSent ? (
+        <>
+          <form onSubmit={handleLoginWithPassword} className='my-5 flex w-full'>
+            <div className='w-full space-y-3 rounded-lg'>
+              <InputText
+                icon='at'
+                labelText='Email'
+                type='email'
+                placeholder='Enter your email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <InputText
+                icon='lock'
+                labelText='Password'
+                type='password'
+                placeholder='Enter your password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <div className='w-full rounded-lg pt-3'>
+                <SubmitButton
+                  variant='primary'
+                  type='submit'
+                  text='Login'
+                  name='login'
+                  className='w-full'
+                />
+              </div>
+            </div>
+          </form>
+          <div>
+            <Text variant='caption' className='text-center opacity-50'>
+              Or
+            </Text>
+          </div>
+          <Button
+            variant='secondary'
+            text='Login with OTP'
+            onClick={() => sendOTP(email)}
+          />
+        </>
+      ) : (
+        <form onSubmit={handleVerifyOTP} className='my-5 flex w-full'>
+          <div className='flex w-full flex-col items-center justify-center space-y-3 rounded-lg'>
+            <div className='flex w-full justify-center'>
+              <OtpInput
+                value={otp}
+                onChange={setOTP}
+                numInputs={6}
+                renderSeparator={<div className='mx-1'></div>}
+                inputType='number'
+                shouldAutoFocus={true}
+                renderInput={(props, index) => {
+                  return (
+                    <input
+                      {...props}
+                      className='rounded-lg border border-zinc-200 bg-transparent bg-zinc-100 p-2 text-gray-700 shadow-sm outline-none dark:border-zinc-700 dark:bg-zinc-800 dark:text-gray-200'
+                      maxLength={1}
+                      style={{
+                        width: '2em',
+                        textAlign: 'center',
+                        fontSize: '1.5em',
+                      }}
+                    />
+                  );
+                }}
+              />
+            </div>
+            <SubmitButton
+              variant='primary'
+              type='submit'
+              text='Verify'
+              name='verify'
+            />
+          </div>
+        </form>
+      )}
       {message && (
         <Card className='px-5 py-3'>
           <Text variant='caption' className='text-center opacity-50'>
@@ -135,38 +224,3 @@ export const OTPHandler = () => {
     </div>
   );
 };
-
-// <form onSubmit={handleVerifyOTP} className='my-5 flex w-full'>
-//           <div className='flex w-full flex-col items-center justify-center space-y-3 rounded-lg'>
-//             <div className='flex w-full justify-center'>
-//               <OtpInput
-//                 value={otp}
-//                 onChange={setOTP}
-//                 numInputs={6}
-//                 renderSeparator={<div className='mx-1'></div>}
-//                 inputType='number'
-//                 shouldAutoFocus={true}
-//                 renderInput={(props, index) => {
-//                   return (
-//                     <input
-//                       {...props}
-//                       className='rounded-lg border border-zinc-200 bg-transparent bg-zinc-100 p-2 text-gray-700 shadow-sm outline-none dark:border-zinc-700 dark:bg-zinc-800 dark:text-gray-200'
-//                       maxLength={1}
-//                       style={{
-//                         width: '2em',
-//                         textAlign: 'center',
-//                         fontSize: '1.5em',
-//                       }}
-//                     />
-//                   );
-//                 }}
-//               />
-//             </div>
-//             <SubmitButton
-//               variant='primary'
-//               type='submit'
-//               text='Verify'
-//               name='verify'
-//             />
-//           </div>
-//         </form>
