8849d53f1ccb0e1b64e26dd3fe0a8351cddce0f7
CodenameWaddles <clovis.oudart@wanadoo.fr>
2023-11-23 19:35:27 +0100

    Auth UI WIP

--- mobile/components/auth/AuthCard.tsx
+++ mobile/components/auth/AuthCard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {View} from "../Themed";
 import {Button, TextInput} from "react-native";
 import {useAuth} from "./AuthProvider";
+import {Button as Button2} from "../common/Button";
 
 export function AuthCard() {
     const [email, setEmail] = useState<string>('');
@@ -21,6 +22,7 @@ export function AuthCard() {
             <TextInput placeholder="Email" onChangeText={handleEmailChange}></TextInput>
             <TextInput placeholder="Password" onChangeText={handlePasswordChange}></TextInput>
             <Button title={"Sign in"} onPress={() => signIn(email, password)} />
+            <Button2 text={"Sign in"} onPress={() => signIn(email, password)} variant={"primary"} />
         </View>
     )
 }
