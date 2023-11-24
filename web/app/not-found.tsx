3bce275d7b0a028a53d619bdf347f9a75035bf88
victorbillaud <victorbillaud@gmail.com>
2023-11-24 15:56:31 +0100

    fix node_module error

--- web/app/not-found.tsx
+++ web/app/not-found.tsx
@@ -1,13 +1,7 @@
 import { StyledLink } from '@/components/common/link';
 import { Text } from '@/components/common/text';
-import { createClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers';
 
 export default async function NotFound() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  const { data } = await supabase.auth.getUser();
-
   return (
     <div className='animate-in flex flex-1 flex-col items-center justify-center gap-5 px-3 opacity-0'>
       <Text variant={'h1'} className='text-center opacity-50'>
