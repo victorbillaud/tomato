0776c78301a8c1233bcb1beb88da2655107da1e1
victorbillaud <victorbillaud@gmail.com>
2023-12-04 17:29:25 +0100

    Fix forget password

--- web/app/auth/layout.tsx
+++ web/app/auth/layout.tsx
@@ -1,6 +1,5 @@
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export default async function AuthLayout({
   children,
@@ -12,11 +11,7 @@ export default async function AuthLayout({
   const {
     data: { user },
   } = await supabase.auth.getUser();
-
-  if (user) {
-    redirect('/user');
-  }
-
+  
   return (
     <main className='flex w-full max-w-lg flex-1 flex-col items-center justify-center'>
       {children}
