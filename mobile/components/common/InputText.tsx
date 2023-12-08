55ee1f86ee66dd4f36c645a79f6714bf4906e9f0
GuillaumeVDN <vandenneucker.guillaume@gmail.com>
2023-12-08 14:32:37 +0100

    Adapted InputText to mobile

--- mobile/components/common/InputText.tsx
+++ mobile/components/common/InputText.tsx
@@ -1,61 +1,61 @@
-import classNames from 'classnames';
-import React from 'react';
-import { Icon, IconNames } from './Icon';
-import { Text } from './Text';
-import tw from "../../constants/tw";
+import React from 'react'
+import { Icon, IconNames } from './Icon'
+import { Text } from './Text'
+import { View } from "../Themed"
+import { TextInput } from "react-native";
+import tw from "../../constants/tw"
+
 interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    labelText?: string;
-    error?: string;
-    children?: React.ReactNode;
-    icon?: IconNames;
+	labelText?: string
+	error?: string
+	children?: React.ReactNode
+	icon?: IconNames
+	onChangeText?: ((text: string) => void) | undefined;
 }
 
-export const InputText = React.forwardRef<HTMLInputElement, IProps>(
-    ({ className, children, labelText, type = 'text', error, ...props }, ref) => {
-        const inputContainerClass = classNames(
-            'group flex items-stretch gap-2 border shadow-sm',
-            error
-                ? 'animate-shake border border-red-500 bg-red-700/10'
-                : 'border-zinc-200 dark:border-zinc-700 dark:bg-zinc-800 bg-zinc-100',
-            'text-s text-black-100 dark:text-white-100 w-full px-2 py-2 outline-none transition-all lg:text-sm xl:text-base',
-            children ? 'rounded-r-md' : 'rounded-md'
-        );
+export function InputText(props: IProps) {
+	return (
+		<View style={tw`flex w-full flex-col gap-1`}>
+			{/* -------------------- LABEL & ERROR -------------------- */}
+			{props.labelText && (
+				<View style={tw`flex flex-row items-center justify-between gap-3`}>
+					<label style={tw`text-left`} htmlFor='txt'>
+						<Text
+							variant='caption'
+							style={tw`py-0 pl-1 font-medium capitalize`}
+							weight={500}
+						>
+							{props.labelText}
+						</Text>
+					</label>
+					{props.error && (
+						<Text variant='overline' style={tw`text-red-600`}>
+							{props.error}
+						</Text>
+					)}
+				</View>
+			)}
 
-        return (
-            <div className='flex w-full flex-col gap-1'>
-                {labelText && (
-                    <div className='flex flex-row items-center justify-between gap-3'>
-                        <label className='text-left' htmlFor='txt'>
-                            <Text
-                                variant='caption'
-                                style='py-0 pl-1 font-medium capitalize'
-                                weight={500}
-                            >
-                                {labelText}
-                            </Text>
-                        </label>
-                        {error && (
-                            <Text variant='overline' className='text-red-600'>
-                                {error}
-                            </Text>
-                        )}
-                    </div>
-                )}
-                <div className={inputContainerClass}>
-                    {props.icon && <Icon name={props.icon} size={20} />}
-                    <input
-                        id='txt'
-                        autoComplete='off'
-                        className='w-full border-none bg-transparent text-gray-700 outline-none placeholder:opacity-20 group-focus:border-transparent group-focus:outline-none group-focus:ring-2 group-focus:ring-gray-200 dark:text-gray-200'
-                        {...props}
-                        ref={ref}
-                        type={type}
-                    />
-                    <div className='flex'>{children}</div>
-                </div>
-            </div>
-        );
-    }
-);
+			{/* -------------------- ICON & INPUT -------------------- */}
+			<View style={[
+				tw`group flex items-stretch gap-2 border shadow-sm`,
+				props.error
+					? tw`animate-shake border border-red-500 bg-red-700/10`
+					: tw`border-zinc-200 dark:border-zinc-700 dark:bg-zinc-800 bg-zinc-100`,
+				tw`text-s text-black-100 dark:text-white-100 w-full px-2 py-2 outline-none transition-all lg:text-sm xl:text-base`,
+				props.children ? tw`rounded-r-md` : tw`rounded-md`
+			]}>
+				{props.icon && <Icon name={props.icon} size={20} />}
 
-InputText.displayName = 'InputText';
+				<TextInput
+					onChangeText={props.onChangeText}
+					style={tw`w-full border-none bg-transparent text-gray-700 outline-none placeholder:opacity-20 group-focus:border-transparent group-focus:outline-none group-focus:ring-2 group-focus:ring-gray-200 dark:text-gray-200`}
+				/>
+
+				<View style={tw`flex`}>{props.children}</View>
+			</View>
+		</View>
+	)
+}
+
+InputText.displayName = 'InputText'
