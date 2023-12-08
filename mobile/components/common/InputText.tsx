6f4ca5e209d3d1ba9c77303ca0ccd5a93107bdf3
CodenameWaddles <clovis.oudart@wanadoo.fr>
2023-12-08 15:34:08 +0100

    Fixed tw style for react native

--- mobile/components/common/InputText.tsx
+++ mobile/components/common/InputText.tsx
@@ -19,7 +19,7 @@ export function InputText(props: IProps) {
 			{/* -------------------- LABEL & ERROR -------------------- */}
 			{props.labelText && (
 				<View style={tw`flex flex-row items-center justify-between gap-3`}>
-					<label style={tw`text-left`} htmlFor='txt'>
+					<View style={tw`text-left`}>
 						<Text
 							variant='caption'
 							style={tw`py-0 pl-1 font-medium capitalize`}
@@ -27,7 +27,7 @@ export function InputText(props: IProps) {
 						>
 							{props.labelText}
 						</Text>
-					</label>
+					</View>
 					{props.error && (
 						<Text variant='overline' style={tw`text-red-600`}>
 							{props.error}
@@ -38,18 +38,18 @@ export function InputText(props: IProps) {
 
 			{/* -------------------- ICON & INPUT -------------------- */}
 			<View style={[
-				tw`group flex items-stretch gap-2 border shadow-sm`,
+				tw`flex items-stretch gap-2 border shadow-sm`,
 				props.error
-					? tw`animate-shake border border-red-500 bg-red-700/10`
+					? tw`border border-red-500 bg-red-700/10`
 					: tw`border-zinc-200 dark:border-zinc-700 dark:bg-zinc-800 bg-zinc-100`,
-				tw`text-s text-black-100 dark:text-white-100 w-full px-2 py-2 outline-none transition-all lg:text-sm xl:text-base`,
+				tw`text-sm text-black dark:text-white-100 w-full px-2 py-2 lg:text-sm xl:text-base`,
 				props.children ? tw`rounded-r-md` : tw`rounded-md`
 			]}>
 				{props.icon && <Icon name={props.icon} size={20} />}
 
 				<TextInput
 					onChangeText={props.onChangeText}
-					style={tw`w-full border-none bg-transparent text-gray-700 outline-none placeholder:opacity-20 group-focus:border-transparent group-focus:outline-none group-focus:ring-2 group-focus:ring-gray-200 dark:text-gray-200`}
+					style={tw`w-full bg-transparent text-gray-700 placeholder:opacity-20 dark:text-gray-200`}
 				/>
 
 				<View style={tw`flex`}>{props.children}</View>
