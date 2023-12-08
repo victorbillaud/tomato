d291a65bcd8cb3f83ae1f52faaabc223124dea7f
GuillaumeVDN <vandenneucker.guillaume@gmail.com>
2023-12-08 14:48:51 +0100

    Removed old icon component

--- mobile/components/common/Icon.tsx
+++ mobile/components/common/Icon.tsx
@@ -1,42 +1,53 @@
-import { FunctionComponent } from 'react';
-import { IconArrowLeft, IconLoader, IconDiscountCheckFilled, IconDiscountCheck,IconAlertHexagon, IconChevronCompactDown, IconLock, IconAt, IconQrcode, IconQuestionMark, IconDownload, IconWritingSign, IconWritingSignOff, IconCheck, IconChevronDown,IconChevronUp } from '@tabler/icons-react';
+import {
+	IconAlertHexagon,
+	IconArrowLeft,
+	IconAt,
+	IconCheck,
+	IconChevronCompactDown,
+	IconChevronDown,
+	IconChevronUp,
+	IconDiscountCheck,
+	IconDiscountCheckFilled,
+	IconDownload,
+	IconLoader,
+	IconLock,
+	IconQrcode,
+	IconQuestionMark,
+	IconWritingSign,
+	IconWritingSignOff
+} from '@tabler/icons-react'
 
 const ICONS = {
-    'arrow-left': IconArrowLeft,
-    'loader': IconLoader,
-    'discount-check': IconDiscountCheckFilled,
-    'discount-check-outline': IconDiscountCheck,
-    'alert': IconAlertHexagon,
-    'chevron-compact-down': IconChevronCompactDown,
-    'lock': IconLock,
-    'at': IconAt,
-    'qrcode': IconQrcode,
-    'question-mark': IconQuestionMark,
-    'download': IconDownload,
-    'pencil': IconWritingSign,
-    'pencil-off': IconWritingSignOff,
-    'check': IconCheck,
-    'chevron-down': IconChevronDown,
-    'chevron-up': IconChevronUp,
-};
-
-export type IconNames = keyof typeof ICONS;
-export interface IIconProps {
-    name: IconNames;
-    size?: number;
-    color?: string;
-    stroke?: number;
+	'alert': IconAlertHexagon,
+	'arrow-left': IconArrowLeft,
+	'at': IconAt,
+	'check': IconCheck,
+	'chevron-compact-down': IconChevronCompactDown,
+	'chevron-down': IconChevronDown,
+	'chevron-up': IconChevronUp,
+	'discount-check': IconDiscountCheckFilled,
+	'discount-check-outline': IconDiscountCheck,
+	'download': IconDownload,
+	'qrcode': IconQrcode,
+	'question-mark': IconQuestionMark,
+	'loader': IconLoader,
+	'lock': IconLock,
+	'pencil': IconWritingSign,
+	'pencil-off': IconWritingSignOff,
 }
 
-export const Icon: FunctionComponent<IIconProps> = ({
-            name,
-            size = 24,
-            color = 'black',
-            stroke = 2,
-        }) => {
-    const Icon = ICONS[name];
+export type IconNames = keyof typeof ICONS
 
-    return <Icon name={name} size={size} color={color} stroke={stroke} />;
-};
+export interface IIconProps {
+	name: IconNames
+	size?: number
+	color?: string
+	stroke?: number
+}
 
-export default Icon;
+export function Icon({ name, size = 24, color = 'black', stroke = 2, }: IIconProps) {
+	const Icon = ICONS[name]
+	return <Icon name={name} size={size} color={color} stroke={stroke} />
+}
+
+export default Icon
