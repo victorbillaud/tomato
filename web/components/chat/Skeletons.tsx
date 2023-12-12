52e09ebe127b2724b618732783a1d0bc60b6a202
Thomas <tomspace.800@gmail.com>
2023-12-12 12:56:30 +0100

    fix: better skeleton rendering

--- web/components/chat/Skeletons.tsx
+++ web/components/chat/Skeletons.tsx
@@ -1,7 +1,9 @@
+const skeletonStyle = 'animate-pulse bg-gray-300/50 dark:bg-gray-300/20';
+
 export const ChatListSkeleton = () => {
   return (
-    <div className='flex h-full w-full flex-col gap-2 pr-4'>
-      <div className='h-6 w-1/2 animate-pulse rounded-lg bg-gray-300/20 pb-6'></div>
+    <div className={`flex h-full w-full flex-col gap-2 pr-4`}>
+      <div className={`${skeletonStyle} h-6 w-1/2 rounded-lg pb-6`}></div>
       {[...Array(8)].map((_, index) => (
         <ChatCardSkeleton key={index} />
       ))}
@@ -13,17 +15,17 @@ export const ChatCardSkeleton = () => {
   return (
     <div className='h-18 flex w-full items-center gap-2 px-2 py-3'>
       <div className='flex-shrink-0'>
-        <div className='h-10 w-10 animate-pulse rounded-full bg-gray-300/20'></div>
+        <div className={`${skeletonStyle} h-10 w-10 rounded-full`}></div>
       </div>
 
       <div className='flex flex-grow flex-col gap-2'>
-        <div className='h-5 w-full animate-pulse rounded-lg bg-gray-300/20'></div>
-        <div className='h-4 w-1/2 animate-pulse rounded-lg bg-gray-300/20'></div>
+        <div className={`${skeletonStyle} h-5 w-full rounded-lg`}></div>
+        <div className={`${skeletonStyle} h-4 w-1/2 rounded-lg`}></div>
       </div>
 
       <div className='flex flex-shrink-0 flex-col items-end gap-2'>
-        <div className='h-5 w-12 animate-pulse rounded-lg bg-gray-300/20'></div>
-        <div className='h-4 w-16 animate-pulse rounded-lg bg-gray-300/20'></div>
+        <div className={`${skeletonStyle} h-5 w-12 rounded-lg`}></div>
+        <div className={`${skeletonStyle} h-4 w-16 rounded-lg`}></div>
       </div>
     </div>
   );
@@ -32,17 +34,29 @@ export const ChatCardSkeleton = () => {
 export const ChatSkeleton = () => {
   return (
     <div className='flex h-full w-full flex-col gap-3 pb-6 sm:pl-6'>
-      <div className='h-12 w-1/3 animate-pulse self-start rounded-lg bg-gray-300/20'></div>
-      <div className='h-12 w-2/3 animate-pulse self-start rounded-lg bg-gray-300/20'></div>
-      <div className='h-12 w-2/3 animate-pulse self-end rounded-lg bg-gray-300/20'></div>
-      <div className='h-12 w-1/2 animate-pulse self-start rounded-lg bg-gray-300/20'></div>
-      <div className='h-12 w-1/2 animate-pulse self-end rounded-lg bg-gray-300/20'></div>
-      <div className='h-12 w-1/3 animate-pulse self-end rounded-lg bg-gray-300/20'></div>
-      <div className='h-12 w-1/5 animate-pulse self-start rounded-lg bg-gray-300/20'></div>
-      <div className='h-12 w-1/2 animate-pulse self-start rounded-lg bg-gray-300/20'></div>
-      <div className='h-12 w-1/4 animate-pulse self-start rounded-lg bg-gray-300/20'></div>
-      <div className='h-24 w-2/3 animate-pulse self-end rounded-lg bg-gray-300/20'></div>
-      <div className='h-12 w-1/2 animate-pulse self-end rounded-lg bg-gray-300/20'></div>
+      <div
+        className={`${skeletonStyle} h-12 w-1/3 self-start rounded-lg`}
+      ></div>
+      <div
+        className={`${skeletonStyle} h-12 w-2/3 self-start rounded-lg`}
+      ></div>
+      <div className={`${skeletonStyle} h-12 w-2/3 self-end rounded-lg`}></div>
+      <div
+        className={`${skeletonStyle} h-12 w-1/2 self-start rounded-lg`}
+      ></div>
+      <div className={`${skeletonStyle} h-12 w-1/2 self-end rounded-lg`}></div>
+      <div className={`${skeletonStyle} h-12 w-1/3 self-end rounded-lg`}></div>
+      <div
+        className={`${skeletonStyle} h-12 w-1/5 self-start rounded-lg`}
+      ></div>
+      <div
+        className={`${skeletonStyle} h-12 w-1/2 self-start rounded-lg`}
+      ></div>
+      <div
+        className={`${skeletonStyle} h-12 w-1/4 self-start rounded-lg`}
+      ></div>
+      <div className={`${skeletonStyle} h-24 w-2/3 self-end rounded-lg`}></div>
+      <div className={`${skeletonStyle} h-12 w-1/2 self-end rounded-lg`}></div>
     </div>
   );
 };
@@ -50,7 +64,7 @@ export const ChatSkeleton = () => {
 export const InputSkeleton = () => {
   return (
     <div className='h-full'>
-      <div className='h-12 w-full animate-pulse rounded-lg bg-gray-300/20'></div>
+      <div className={`${skeletonStyle} h-12 w-full rounded-lg`}></div>
     </div>
   );
 };
