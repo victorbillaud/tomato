a54114a782e763f02a3d9d46b476efe3a1ca7e02
guillaumevdn <vandenneucker.guillaume@gmail.com>
2023-12-16 12:38:09 +0100

    cleanup: AuthProvider code

--- mobile/components/auth/AuthProvider.tsx
+++ mobile/components/auth/AuthProvider.tsx
@@ -7,20 +7,19 @@ import { useSupabase } from "@/components/supabase/SupabaseProvider";
 interface AuthContextType {
 	user: User | null
 	loading: boolean
-	signIn: (email: string, password: string) => Promise<void>
+	signIn: (email: string, password: string) => Promise<boolean>
 	sendOTP: (email: string) => Promise<boolean>
-	signInWithOTP: (email: string, otp: string) => Promise<void>
-	//signInWithProvider: (provider: Provider) => Promise<void>
+	signInWithOTP: (email: string, otp: string) => Promise<boolean>
 	signOut: () => Promise<void>
 }
 
 const AuthContext = createContext<AuthContextType>({} as AuthContextType)
 
-interface AuthProviderProps {
+interface Props {
 	children: ReactNode
 }
 
-export function AuthProvider({ children }: AuthProviderProps) {
+export function AuthProvider(props: Props) {
 	const [user, setUser] = useState<User | null>(null)
 	const [loading, setLoading] = useState<boolean>(true)
 	const supabase = useSupabase()
@@ -44,67 +43,59 @@ export function AuthProvider({ children }: AuthProviderProps) {
 		return () => { listener.subscription?.unsubscribe() }
 	}, [])
 
+	async function signIn(email: string, password: string): Promise<boolean> {
+		const { data, error } = await supabase.auth.signInWithPassword({ email, password })
+		if (error) {
+			if (error.message === 'Invalid login credentials') {InvalidCredentialsAlert()} else {console.error(error)}
+			return false
+		}
+		setUser(data?.user ?? null)
+		return true
+	}
+
+	async function sendOTP(email: string): Promise<boolean> {
+		const { data, error } = await supabase.auth.signInWithOtp({
+			email,
+			options: {
+				shouldCreateUser: true,
+			},
+		})
+		if (error) console.error(error)
+		return !error
+	}
+
+	async function signInWithOTP(email: string, otp: string): Promise<boolean> {
+		const { data, error } = await supabase.auth.verifyOtp({
+			email,
+			token: otp,
+			type: 'email',
+		})
+		if (error) {
+			console.error(error)
+			return false
+		}
+		setUser(data?.user ?? null)
+		return true
+	}
+
+	async function signOut(): Promise<void> {
+		const error = await supabase.auth.signOut()
+		if (error) console.error(error)
+		setUser(null)
+	}
+
 	const InvalidCredentialsAlert = () => Alert.alert('Invalid credentials')
 
 	return (
-		<AuthContext.Provider value={{
-			user,
-			loading,
-			signIn: async (email: string, password: string) => {
-				const { data, error } = await supabase.auth.signInWithPassword({ email, password });
-				if (error) {
-					if (error.message === 'Invalid login credentials') {InvalidCredentialsAlert()} else {console.error(error)}
-					return
-				}
-				setUser(data?.user ?? null);
-			},
-			sendOTP: async (email: string) => {
-				const { data, error } = await supabase.auth.signInWithOtp({
-					email,
-					options: {
-						shouldCreateUser: true,
-					},
-				});
-				if (error) {
-					console.error(error)
-					return false
-				}
-				return true
-			},
-			signInWithOTP: async (email: string, otp: string) => {
-				const { data, error } = await supabase.auth.verifyOtp({
-					email,
-					token: otp,
-					type: 'email',
-				});
-				if (error) {
-					console.error(error)
-					return
-				}
-				setUser(data?.user ?? null);
-			},
-			/*signInWithProvider: async (provider: Provider) => {
-				const {error} = await supabase.auth.signInWithOAuth({ provider })
-				if (error) {
-					console.error(error)
-					return
-				}
-			},*/
-			signOut: async () => {
-				await supabase.auth.signOut()
-				setUser(null);
-			},
-		}}>
+		<AuthContext.Provider value={{ user, loading, signIn, sendOTP, signInWithOTP, signOut, }}>
 			{loading ?
 				<Text variant={'caption'}>Loading...</Text>
-				: children
+				: props.children
 			}
 		</AuthContext.Provider>
 	)
 }
 
-export default AuthContext
-
-export const useAuth = () => {
+export function useAuth() {
 	return useContext(AuthContext)
 }
