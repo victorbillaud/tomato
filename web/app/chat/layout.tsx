3cad7dfed7ded4024d12783166e9776cbb48521b
Thomas <tomspace.800@gmail.com>
2023-12-18 12:49:13 +0100

    fix: empty conversations list handling

--- web/app/chat/layout.tsx
+++ web/app/chat/layout.tsx
@@ -1,5 +1,7 @@
 import { ChatProvider } from '@/components/chat/ChatContext';
 import ChatList from '@/components/chat/ChatList';
+import { StyledLink } from '@/components/common/link';
+import { Text } from '@/components/common/text';
 import { createClient } from '@/utils/supabase/server';
 import { User } from '@supabase/supabase-js';
 import { listUserConversations } from '@utils/lib/messaging/services';
@@ -31,11 +33,27 @@ export default async function ChatLayout({
   }
 
   return (
-    <div className='flex h-[80vh] w-full overflow-hidden'>
-      <ChatProvider>
-        <ChatList conversations={conversations} currentUser={user as User} />
-        {children}
-      </ChatProvider>
-    </div>
+    <ChatProvider>
+      {conversations.length > 0 ? (
+        <div className='flex h-[80vh] w-full overflow-hidden'>
+          <ChatList conversations={conversations} currentUser={user as User} />
+          {children}
+        </div>
+      ) : (
+        <div className='my-10 flex w-full max-w-[700px] flex-col items-center justify-start gap-10 px-3 text-center'>
+          <Text variant='h3'>You don&apos;t have any conversations yet</Text>
+          <Text variant='body' className='mx-5'>
+            Found a lost object? Scan its QR Code to start a chat with its owner
+            ! Or, if you&apos;ve lost something, wait for someone to find it and
+            contact you!
+          </Text>
+          <StyledLink
+            variant='secondary'
+            text='Return home'
+            href='/'
+          ></StyledLink>
+        </div>
+      )}
+    </ChatProvider>
   );
 }
