aee62b9f3bd9cbf1303033bd74aa4e219982c215
victorbillaud <victorbillaud@gmail.com>
2023-12-19 11:34:33 +0100

    fix item permissions

--- utils/lib/item/services.ts
+++ utils/lib/item/services.ts
@@ -1,114 +1,142 @@
-import { SupabaseClient } from "@supabase/supabase-js";
-import { associateQRCodeToItem } from "../qrcode/services";
-import { Database } from "../supabase/supabase_types";
+import { SupabaseClient } from '@supabase/supabase-js';
+import { associateQRCodeToItem } from '../qrcode/services';
+import { Database } from '../supabase/supabase_types';
 
 export async function insertItem(
-    supabaseInstance: SupabaseClient<Database>,
-    item: Pick<Database["public"]["Tables"]["item"]["Insert"], "name" | "description" | "qrcode_id" | "activated">
+  supabaseInstance: SupabaseClient<Database>,
+  item: Pick<
+    Database['public']['Tables']['item']['Insert'],
+    'name' | 'description' | 'qrcode_id' | 'activated'
+  >
 ) {
+  const {
+    data: { user },
+  } = await supabaseInstance.auth.getUser();
 
-    const { data: { user } } = await supabaseInstance.auth.getUser();
+  const itemToInsert: Database['public']['Tables']['item']['Insert'] = {
+    user_id: user.id,
+    ...item,
+  };
 
-    const itemToInsert: Database["public"]["Tables"]["item"]["Insert"] = {
-        user_id: user.id,
-        ...item,
-    };
+  const { data: insertedItem, error: itemInsertionError } =
+    await supabaseInstance
+      .from('item')
+      .insert(itemToInsert)
+      .select('*')
+      .single();
 
-    const { data: insertedItem, error: itemInsertionError } = await supabaseInstance
-        .from('item')
-        .insert(itemToInsert)
-        .select('*')
-        .single();
+  if (itemInsertionError) {
+    return { insertedItem, error: itemInsertionError };
+  }
 
-    if (itemInsertionError) {
-        return { insertedItem, error: itemInsertionError }
-    }
+  const { error: qrCodeUpdateError } = await associateQRCodeToItem(
+    supabaseInstance,
+    insertedItem.qrcode_id,
+    insertedItem.id
+  );
 
-    const { error: qrCodeUpdateError } = await associateQRCodeToItem(supabaseInstance, insertedItem.qrcode_id, insertedItem.id);
+  if (qrCodeUpdateError) {
+    return { insertedItem, error: qrCodeUpdateError };
+  }
 
-    if (qrCodeUpdateError) {
-        return { insertedItem, error: qrCodeUpdateError }
-    }
-
-    return { insertedItem, error: null };
+  return { insertedItem, error: null };
 }
 
 export async function listItems(supabaseInstance: SupabaseClient<Database>) {
-    const { data, error } = await supabaseInstance
-        .from('item')
-        .select(`
+  const {
+    data: { user },
+  } = await supabaseInstance.auth.getUser();
+
+  const { data, error } = await supabaseInstance
+    .from('item')
+    .select(
+      `
             *,
             qrcode!qrcode_item_id_fkey (
                 *
             )
-        `);
+        `
+    )
+    .eq('user_id', user.id);
 
-    return { data, error }
+  return { data, error };
 }
 
 export async function getItemFromQrCodeId(
-    supabaseInstance: SupabaseClient<Database>,
-    qrCodeId: string
+  supabaseInstance: SupabaseClient<Database>,
+  qrCodeId: string
 ) {
-    const { data, error } = await supabaseInstance
-        .from('item')
-        .select(`
+  const { data, error } = await supabaseInstance
+    .from('item')
+    .select(
+      `
             *,
             qrcode!qrcode_item_id_fkey (
                 *
             )
-        `)
-        .eq('qrcode_id', qrCodeId)
-        .limit(1)
-        .single();
+        `
+    )
+    .eq('qrcode_id', qrCodeId)
+    .limit(1)
+    .single();
 
-    return { data, error }
+  return { data, error };
 }
 
 export async function getItem(
-    supabaseInstance: SupabaseClient<Database>,
-    itemId: string
+  supabaseInstance: SupabaseClient<Database>,
+  itemId: string
 ) {
-    const { data, error } = await supabaseInstance
-        .from('item')
-        .select(`
+  const {
+    data: { user },
+  } = await supabaseInstance.auth.getUser();
+
+  const { data, error } = await supabaseInstance
+    .from('item')
+    .select(
+      `
             *,
             qrcode!qrcode_item_id_fkey (
                 *
             )
-        `)
-        .eq('id', itemId)
-        .limit(1)
-        .single();
+        `
+    )
+    .eq('id', itemId)
+    .eq('user_id', user.id)
+    .limit(1)
+    .single();
 
-    return { data, error }
+  return { data, error };
 }
 
 export async function activateItem(
-    supabaseInstance: SupabaseClient<Database>,
-    itemId: string
+  supabaseInstance: SupabaseClient<Database>,
+  itemId: string
 ) {
-    const { data, error } = await supabaseInstance
-        .from('item')
-        .update({ activated: true })
-        .eq('id', itemId)
-        .select('*')
-        .single();
+  const { data, error } = await supabaseInstance
+    .from('item')
+    .update({ activated: true })
+    .eq('id', itemId)
+    .select('*')
+    .single();
 
-    return { data, error }
+  return { data, error };
 }
 
 export async function updateItem(
-    supabaseInstance: SupabaseClient<Database>,
-    itemId: string,
-    item: Omit<Database["public"]["Tables"]["item"]["Update"], "id" | "created_at" | "user_id" | "activated">
+  supabaseInstance: SupabaseClient<Database>,
+  itemId: string,
+  item: Omit<
+    Database['public']['Tables']['item']['Update'],
+    'id' | 'created_at' | 'user_id' | 'activated'
+  >
 ) {
-    const { data, error } = await supabaseInstance
-        .from('item')
-        .update(item)
-        .eq('id', itemId)
-        .select('*')
-        .single();
+  const { data, error } = await supabaseInstance
+    .from('item')
+    .update(item)
+    .eq('id', itemId)
+    .select('*')
+    .single();
 
-    return { data, error }
+  return { data, error };
 }
