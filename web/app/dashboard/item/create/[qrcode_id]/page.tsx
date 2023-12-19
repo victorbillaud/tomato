2a422880028afed5fd28fe92bd57ed93534ae696
victorbillaud <victorbillaud@gmail.com>
2023-12-19 19:49:23 +0100

    wip

--- web/app/dashboard/item/create/[qrcode_id]/page.tsx
+++ web/app/dashboard/item/create/[qrcode_id]/page.tsx
@@ -1,4 +1,5 @@
 import { StyledLink } from '@/components/common/link';
+import { Text } from '@/components/common/text';
 
 export default async function CreateItem({
   params,
@@ -6,19 +7,39 @@ export default async function CreateItem({
   params: { qrcode_id: string };
 }) {
   return (
-    <div className='flex w-full flex-1 flex-col items-center justify-center gap-10'>
-      <div className='flex flex-row items-center justify-center gap-5'>
-        <StyledLink
-          text=' Create item from QR Code'
-          variant='secondary'
-          href={`/dashboard/item/create/${params.qrcode_id}/qrcode`}
-        />
-
-        <StyledLink
-          text='Create item from form'
-          variant='secondary'
-          href={`/dashboard/item/create/${params.qrcode_id}/form`}
-        />
+    <div className='flex w-full flex-1 flex-col items-start justify-center gap-10'>
+      <Text variant='h1'>Welcome to the Item Creation page!</Text>
+      <Text variant='h3' weight={300} className='opacity-70'>
+        Here, you can choose how to create your new item. Select one of the
+        methods below to get started.
+      </Text>
+      <div className='flex flex-col items-center justify-center divide-y-2 divide-gray-200/20'>
+        <div className='flex flex-col items-center justify-center gap-5 px-5'>
+          <Text variant='h3'>Creation using the form</Text>
+          <Text variant='body'>
+            Use our online form to enter details about your item quickly and
+            easily. Ideal for those who prefer typing and have all the
+            information ready.
+          </Text>
+          <StyledLink
+            text='Create with your mobile'
+            variant='tertiary'
+            href={`/dashboard/item/create/${params.qrcode_id}/form`}
+          />
+        </div>
+        <div className='flex flex-col items-center justify-center gap-5 px-5'>
+          <Text variant='h3'>Creation using your mobile</Text>
+          <Text variant='body'>
+            Prefer using your phone? Simply scan the QR code and complete the
+            item creation process on your mobile device. Perfect for on-the-go
+            creation.
+          </Text>
+          <StyledLink
+            text=' Create item from QR Code'
+            variant='tertiary'
+            href={`/dashboard/item/create/${params.qrcode_id}/qrcode`}
+          />
+        </div>
       </div>
     </div>
   );
