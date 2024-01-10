5d79533e0cc954326b1c7ffde21685304c0973ad
victorbillaud <victorbillaud@gmail.com>
2024-01-10 21:27:50 +0100

    add sentry

--- web/app/global-error.tsx
+++ web/app/global-error.tsx
@@ -3,6 +3,9 @@
 import { Button } from '@/components/common/button';
 import { Icon } from '@/components/common/icon';
 import { Text } from '@/components/common/text';
+import * as Sentry from "@sentry/nextjs";
+import Error from "next/error";
+import { useEffect } from "react";
 
 export default function GlobalError({
   error,
@@ -11,6 +14,10 @@ export default function GlobalError({
   error: Error & { digest?: string; message: any };
   reset: () => void;
 }) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+  
   return (
     <div className='flex w-full flex-1 flex-col items-center justify-start px-3'>
       <div className='my-10 flex w-full max-w-[700px] items-center justify-between gap-3 rounded-md border border-red-400 bg-red-600/20 px-6 py-3 shadow-sm'>
