bea84af94543a790104f53c5c958eb82b8a00d7c
victorbillaud <victorbillaud@gmail.com>
2024-01-12 12:17:03 +0100

    add public url

--- utils/lib/qrcode/generator.ts
+++ utils/lib/qrcode/generator.ts
@@ -1,5 +1,5 @@
 const generateQRCode = async (data: string): Promise<string> => {
-  const baseUrl = process.env.NEXT_PUBLIC_QR_SERVER_URL;    
+  const baseUrl = process.env.NEXT_PUBLIC_QR_SERVER_URL || 'https://qr-server-victor-freelance.koyeb.app/generate'  
   const url = new URL(baseUrl);
   url.searchParams.append('url', data);
   const response = await fetch(url.toString());
