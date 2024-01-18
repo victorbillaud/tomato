88a3ebc0befd0add95bb67588a78b57f5a99efae
CodenameWaddles <clovis.oudart@wanadoo.fr>
2024-01-18 14:42:28 +0100

    Added chatProvider to mobile layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -7,6 +7,7 @@ import { useColorScheme } from 'react-native'
 import { AuthProvider } from "@/components/auth/AuthProvider";
 import { AuthGuard } from "@/components/auth/AuthGuard";
 import { ItemsProvider } from "@/components/item/ItemsProvider";
+import {ChatsProvider} from "@/components/chat/ChatsProvider";
 
 export { ErrorBoundary } from 'expo-router'
 
@@ -44,7 +45,9 @@ function RootLayoutNav() {
 		<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
 			<AuthProvider>
 				<ItemsProvider>
-					<AuthGuard />
+					<ChatsProvider>
+						<AuthGuard />
+					</ChatsProvider>
 				</ItemsProvider>
 			</AuthProvider>
 		</ThemeProvider>
