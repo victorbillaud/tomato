aea2579dc91f5ed525372831400ff8ee2ecab8a8
Arthur <pigeon.arthur@hotmail.fr>
2024-01-19 13:20:30 +0100

    feat: add possibility to delete image + fix image size bugs

--- web/components/common/input/InputFileForm.tsx
+++ web/components/common/input/InputFileForm.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { forwardRef, useRef } from 'react';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 import { Icon, IconNames } from '../icon';
 import { CustomImage } from '../image';
 // @ts-ignore
 import { useFormStatus } from 'react-dom';
+import Compressor from 'compressorjs';
 
 interface InputFileFormProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -22,21 +23,49 @@ const InputFileFormContent = forwardRef<
 >(({ iconName, imgSource, ...props }, ref) => {
   const { pending } = useFormStatus();
   const buttonRef = useRef<HTMLButtonElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!pending && fileInputRef.current) {
+      fileInputRef.current.value = '';
+    }
+  }, [pending]);
 
   if (!iconName) {
     iconName = 'photo-edit';
   }
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.item(0) || null;
+
+    if (file) {
+      new Compressor(file, {
+        quality: 0.6,
+        convertSize: 700000,
+
+        success(result) {
+          const list = new DataTransfer();
+          list.items.add(new File([result], file.name, { type: file.type }) as any);
+          e.target.files = list.files;
+          buttonRef.current?.click();
+        },
+        error(err) {
+          console.log('Cannot upload the image:', err.message);
+        },
+      });
+    }
+  };
 
   return (
     <>
       <input
         type='file'
         name='picture'
-        onChange={() => buttonRef.current?.click()}
+        onChange={handleImageChange}
         accept='image/jpeg,image/png,image/webp'
         className='absolute z-20 h-full w-full cursor-pointer bg-transparent opacity-0'
         disabled={pending}
-        ref={ref}
+        ref={fileInputRef}
         {...props}
       />
       <button type='submit' className='hidden' ref={buttonRef} />
@@ -51,12 +80,22 @@ const InputFileFormContent = forwardRef<
           </div>
         </div>
       ) : (
-        <Icon
-          name={iconName}
-          size={30}
-          className='absolute left-1/2 top-1/2 z-10 hidden -translate-x-1/2 -translate-y-1/2 group-hover:block'
-          color='text-stone-900 dark:text-white'
-        />
+        <>
+          <Icon
+            name={iconName}
+            size={30}
+            className='absolute left-1/2 top-1/2 z-10 hidden -translate-x-1/2 -translate-y-1/2 group-hover:block'
+            color='text-stone-900 dark:text-white'
+          />
+          <button className='absolute right-0 top-0 z-30 hidden p-1 group-hover:block'>
+            <Icon
+              name='x'
+              size={26}
+              className='hover:text-primary-600'
+              color='text-stone-900 dark:text-white'
+            />
+          </button>
+        </>
       )}
       <CustomImage
         alt='item'
@@ -77,7 +116,7 @@ InputFileFormContent.displayName = 'InputFileFormContent';
 export const InputFileForm = forwardRef<HTMLInputElement, InputFileFormProps>(
   ({ callback, ...props }, ref) => {
     return (
-      <form className='group relative mt-2' action={callback}>
+      <form className='group relative mt-2 w-fit h-fit' action={callback}>
         <InputFileFormContent {...props} ref={ref} />
       </form>
     );
