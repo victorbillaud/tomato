0e10d9369e37844b8003cede8932c8965e9b8dcb
CodenameWaddles <clovis.oudart@wanadoo.fr>
2024-01-19 12:52:05 +0100

    ChatsProviders now gets all chat card content

--- mobile/components/chat/ChatsProvider.tsx
+++ mobile/components/chat/ChatsProvider.tsx
@@ -1,19 +1,21 @@
 import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react'
 import { useSupabase } from "@/components/supabase/SupabaseProvider";
-import { fetchItems, Item } from "@/utils/supabase/items";
+import { getItem } from "@/utils/supabase/items";
 import {User} from "@supabase/supabase-js";
 import {listUserConversations, TConversationWithLastMessage} from "@/utils/supabase/messaging";
+import {getUserDetails} from "@/utils/supabase/user";
 import { useAuth } from "@/components/auth/AuthProvider";
+import {Database} from "@/utils/supabase/supabase_types";
 
 interface ChatsContextType {
-    conversations: TConversationWithLastMessage[]
+    conversationCards: ChatCardContent[]
     loading: boolean
 }
 
-const ChatsContext = createContext<ChatsContextType>({ conversations: [], loading: false })
+const ChatsContext = createContext<ChatsContextType>({ conversationCards: [], loading: false })
 
 export function ChatsProvider({ ...props }: { children: ReactNode }) {
-    const [conversations, setConversations] = useState<TConversationWithLastMessage[]>([])
+    const [conversationCards, setConversationCards] = useState<ChatCardContent[]>([])
     const [loading, setLoading] = useState<boolean>(true)
     const supabase = useSupabase()
     const user = useAuth().user
@@ -22,9 +24,21 @@ export function ChatsProvider({ ...props }: { children: ReactNode }) {
         const init = async () => {
             setLoading(true)
             try {
-                setConversations(await listUserConversations(supabase))
+                let cards : ChatCardContent[] = []
+                let conversations = await listUserConversations(supabase)
+                for (const conversation of conversations) {
+                    let isOwner = conversation.owner_id === user?.id;
+                    let itemId = conversation.item_id;
+                    let conv = conversation
+                    const { user: userDetailsFetched, error: avatarError } =
+                        await getUserDetails(supabase, isOwner ? conversation.finder_id : conversation.owner_id);
+                    const { data: item, error: itemError } =
+                        await getItem(supabase, itemId);
+                    cards.push({conversation : conv, otherUser: userDetailsFetched, itemInfo: item as DBItem})
+                }
+                setConversationCards(cards)
             } catch (error) {
-                setConversations([])
+                setConversationCards([])
                 console.error(error)
             }
             setLoading(false)
@@ -33,7 +47,19 @@ export function ChatsProvider({ ...props }: { children: ReactNode }) {
             init()
     }, [user])
 
-    return <ChatsContext.Provider value={{ conversations, loading }}>{props.children}</ChatsContext.Provider>
+    return <ChatsContext.Provider value={{ conversationCards, loading }}>{props.children}</ChatsContext.Provider>
+}
+
+export type DBMessage = Database['public']['Tables']['message']['Row'];
+
+export type DBProfile = Database['public']['Tables']['profiles']['Row'];
+
+export type DBItem = Database['public']['Tables']['item']['Row'];
+
+export type ChatCardContent = {
+    conversation: TConversationWithLastMessage;
+    otherUser: DBProfile | null;
+    itemInfo: DBItem | null;
 }
 
 export const useChats = () => useContext(ChatsContext)
