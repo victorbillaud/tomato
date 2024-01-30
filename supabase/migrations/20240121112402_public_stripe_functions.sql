7db7a7fb38ea6c021afd1cbc3002ce7f0933cc7e
victorbillaud <victorbillaud@gmail.com>
2024-01-30 10:56:37 +0100

    complete migration with stripe wrappers

--- supabase/migrations/20240121112402_public_stripe_functions.sql
+++ supabase/migrations/20240121112402_public_stripe_functions.sql
@@ -1,7 +1,110 @@
 create extension if not exists "wrappers" with schema "extensions";
 
-
 drop policy "Enable insert for authenticated users only" on "public"."qrcode";
+
+create extension if not exists wrappers with schema extensions;
+
+create foreign data wrapper stripe_wrapper
+  handler stripe_fdw_handler
+  validator stripe_fdw_validator;
+
+create server stripe_server
+  foreign data wrapper stripe_wrapper
+  options (
+    api_key_id (select key_id from vault.secrets where name = 'stripe'), -- Retrieve key_id from the first statement
+    api_url 'https://api.stripe.com/v1/'  -- Stripe API base URL, optional. Default is 'https://api.stripe.com/v1/'
+  );
+
+create schema if not exists "stripe";
+
+create foreign table stripe.accounts (
+  id text,
+  business_type text,
+  country text,
+  email text,
+  type text,
+  created timestamp,
+  attrs jsonb
+)
+  server stripe_server
+  options (
+    object 'accounts'
+  );
+
+create foreign table stripe.checkout_sessions (
+  id text,
+  customer text,
+  payment_intent text,
+  subscription text,
+  attrs jsonb
+)
+  server stripe_server
+  options (
+    object 'checkout/sessions',
+    rowid_column 'id'
+  );
+
+create foreign table stripe.customers (
+  id text,
+  email text,
+  name text,
+  description text,
+  created timestamp,
+  attrs jsonb
+)
+  server stripe_server
+  options (
+    object 'customers',
+    rowid_column 'id'
+  );
+
+create foreign table stripe.invoices (
+  id text,
+  customer text,
+  subscription text,
+  status text,
+  total bigint,
+  currency text,
+  period_start timestamp,
+  period_end timestamp,
+  attrs jsonb
+)
+  server stripe_server
+  options (
+    object 'invoices'
+  );
+
+create foreign table stripe.prices (
+  id text,
+  active bool,
+  currency text,
+  product text,
+  unit_amount bigint,
+  type text,
+  created timestamp,
+  attrs jsonb
+)
+  server stripe_server
+  options (
+    object 'prices'
+  );
+
+create foreign table stripe.products (
+  id text,
+  name text,
+  active bool,
+  default_price text,
+  description text,
+  created timestamp,
+  updated timestamp,
+  attrs jsonb
+)
+  server stripe_server
+  options (
+    object 'products',
+    rowid_column 'id'
+  );
+
 
 set check_function_bodies = off;
 
@@ -80,6 +183,5 @@ with check (true);
 
 
 
-create schema if not exists "stripe";
 
 
